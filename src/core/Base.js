912e2530ff04b10d4bf3ccff02d6b7e74cec6906
anupam227 <anupamagarwal721@gmail.com>
2021-02-01 11:51:22 +0530

    footer changes

--- src/core/Base.js
+++ src/core/Base.js
@@ -16,7 +16,7 @@ const Base = ({
                  </div> 
              <div className={className}>{children}</div>        
              </div>
-             <footer className="footer mt-auto-py-3">
+             <footer className="footer mt-auto-py-3" style={{position:"relative"}} >
                  <div className="container-fluid bg-success text-white text-center py-3">
                      <h4>If you have any question, feel free to reach out!</h4>
                      <button className="btn btn-warning btn-lg">Contact Us</button>
